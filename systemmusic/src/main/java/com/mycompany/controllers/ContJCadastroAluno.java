3145c08fa33d2c967c97d1863ee5d409e7d2cfd7
Carlos Alexandre Torrecilhas da Silva Sá <carlos.sa@ameh.org.br>
2024-10-25 10:45:39 -0300

    Conseguimos instanciar um objeto estudante normalmente

--- systemmusic/src/main/java/com/mycompany/controllers/ContJCadastroAluno.java
+++ systemmusic/src/main/java/com/mycompany/controllers/ContJCadastroAluno.java
@@ -1,15 +1,20 @@
 package com.mycompany.controllers;
 
-    //Importing classes from the project
+   //Importing classes from the project
     import com.mycompany.user.Estudante;
     import com.mycompany.mainpage.JCadastroAluno;
-    //Importing swing classes
+   //Importing swing classes
     import javax.swing.JOptionPane;
     import javax.swing.JButton;
     import javax.swing.JTextField;
     import javax.swing.JComboBox;
-    //Importing SQL classes
+   //Importing SQL classes
     import java.sql.Date;
+   //Importing Date from Java
+
+    import java.time.LocalDate;
+    import java.time.format.DateTimeFormatter;
+    import java.time.format.DateTimeParseException;
 
 
 public class ContJCadastroAluno {
@@ -22,23 +27,28 @@ public class ContJCadastroAluno {
     private JTextField txtTelefone;
     private JTextField txtEmail;
     private JComboBox cBoxInstrumento;
+    private Date dataNascimento;
+    private LocalDate localDate;
+    private DateTimeFormatter formato = DateTimeFormatter.ofPattern("dd/MM/yyyy");
     
     public ContJCadastroAluno(JCadastroAluno jCadastroAluno){
         
-        this.jCadastroAluno = jCadastroAluno;
         this.btnCadastrar = jCadastroAluno.getBtnCadastrar();
-        this.txtNome = jCadastroAluno.getTxtNome();
-        this.txtDataNascimento = jCadastroAluno.getTxtDataNascimento();
-        this.txtTelefone = jCadastroAluno.getTxtTelefone();
-        this.txtEmail = jCadastroAluno.getTxtEmail();
-        this.cBoxInstrumento = jCadastroAluno.getcBoxInstrumento();
         
         this.btnCadastrar.addActionListener(e ->{
             
-            Date date = new Date();
+            this.jCadastroAluno = jCadastroAluno;
+            this.btnCadastrar = jCadastroAluno.getBtnCadastrar();
+            this.txtNome = jCadastroAluno.getTxtNome();
+            this.txtDataNascimento = jCadastroAluno.getTxtDataNascimento();
+            this.txtTelefone = jCadastroAluno.getTxtTelefone();
+            this.txtEmail = jCadastroAluno.getTxtEmail();
+            this.cBoxInstrumento = jCadastroAluno.getcBoxInstrumento();
+            this.localDate = LocalDate.parse(txtDataNascimento.getText(), formato);
+            this.dataNascimento = Date.valueOf(localDate);
             
-            this.estudante = new Estudante(txtNome.getText(), new Date(txtDataNascimento.getText(),cBoxInstrumento.getSelectedItem(), txtTelefone.getText(), txtEmail.getText()));
-            //String nome, Date dataNascimento, String instrumentoDesejado, String telefone ,String email
+            this.estudante = new Estudante(txtNome.getText(), this.dataNascimento, cBoxInstrumento.getSelectedItem().toString(), txtTelefone.getText(), txtEmail.getText());
+            JOptionPane.showMessageDialog(null, this.estudante.getNome());
         });
         
     }
