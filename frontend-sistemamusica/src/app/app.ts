87514607ba8e4816d663fb67207dc17249603b83
AlexandreTorrecilhas <carlos.torrecilhas@outlook.com>
2026-02-21 19:29:42 -0300

    iniciando a borda

--- frontend-sistemamusica/src/app/app.ts
+++ frontend-sistemamusica/src/app/app.ts
@@ -1,9 +1,10 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { PageBorderComponent } from './components/pageborder/pageborder.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, PageBorderComponent],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
